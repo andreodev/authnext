cb804abca246d74f9efcdd9dcd867cd77063adeb
andreowdev <developerandreo@gmail.com>
2025-02-23 22:03:48 -0300

    commit credentials next

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   pages: {
@@ -11,6 +12,27 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "email", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+
+        if(!credentials) {
+          return null
+        }
+        if(credentials.email === "andreohenriqueleite@gmail.com" && credentials.password === "123") {
+          return {
+            id: "1",
+            name: "Andreuu",
+            email: "andreohenriqueleite@gmail.com"
+          }
+        }
+          return null
+        }
+    })
   ],
 };
 
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,17 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function ProfilePage() {
+  const session = await getServerSession()
+
+  if(!session) {
+    redirect("/login")
+  }
 
   return (
     <div>
       <h1>Perfil do Usuário</h1>
+      <h1>{session.user?.name}</h1>
     </div>
   );
 }
